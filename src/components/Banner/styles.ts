3482bb4a6ce86c32b06c72690d95e2663627bc93
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-04-24 17:27:25 -0300

    created components banner and button

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
+import { TagContainer } from '../Tag/styles'
 
 export const Imagem = styled.div`
+  position: relative;
   width: 100%;
   height: 560px;
   display: block;
@@ -8,6 +10,17 @@ export const Imagem = styled.div`
   background-size: cover;
   font-weight: bold;
   padding-top: 340px;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
 `
 
 export const Titulo = styled.h2`
