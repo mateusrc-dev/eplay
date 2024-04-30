1fbbdcd4f23f3c96691af6cc70351dbaa411512e
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-04-29 23:19:53 -0300

    updated project

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,13 +1,29 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TagButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
+  align-items: flex-end;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto; // the width will be distributed of form equal in all elements
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     font-size: 14px;
@@ -17,11 +33,29 @@ export const InputGroup = styled.div`
     color: ${cores.branca};
   }
 
-  input {
+  input,
+  select {
     background-color: ${cores.branca};
     border: 1px solid ${cores.branca};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+export const TabButton = styled.button<TagButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preta)};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding-inline: 8px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
